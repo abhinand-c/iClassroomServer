3d281e0a6040796ab3510042f83d35ad3ea50d0f
Abhinand C <abhinandc.ac@gmail.com>
2020-04-11 18:20:54 +0530

    Update Admin Site

---
+++ iClassroomServer/admin_site.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.views import LoginView
+from django.contrib import admin
+from datetime import datetime
+from app import forms
+
+admin.site.site_header ="F.R.I.D.A.Y"
+admin.site.index_title = "Welcome To Admin Management"
+admin.site.site_title = "Primary Control Hub"
+
+admin.site.login =  LoginView.as_view(
+             template_name='app/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Log in',
+                 'year' : datetime.now().year,
+             }
+         )
