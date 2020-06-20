e04426d014dd17332c395e6f4e488f233bbb3a85
ShilpaRajeev <58106715+ShilpaRajeev@users.noreply.github.com>
2020-06-20 06:45:56 +0530

    Update views.py

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
 
-# Create your views here.
 
 @login_required
 def home(request):
